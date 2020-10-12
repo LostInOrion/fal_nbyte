0499ff1ad23700e548dd8a4dab35f8b23cb4be19
hongguangli <983329309@qq.com>
2020-10-12 17:48:16 +0800

    using FAL

--- stm32f103/applications/main.c
+++ stm32f103/applications/main.c
@@ -12,22 +12,14 @@
 #include <rtdevice.h>
 #include <board.h>
 
-/* defined the LED0 pin: PB1 */
-#define LED0_PIN    GET_PIN(B, 1)
+#include "fal.h"
+
 
 int main(void)
 {
-    int count = 1;
-    /* set LED0 pin mode to output */
-    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
-
-    while (count++)
-    {
-        rt_pin_write(LED0_PIN, PIN_HIGH);
-        rt_thread_mdelay(500);
-        rt_pin_write(LED0_PIN, PIN_LOW);
-        rt_thread_mdelay(500);
-    }
-
+#ifdef PKG_USING_FAL
+    fal_init();
+#endif /*BSK_USING_FAL_INIT*/
+	
     return RT_EOK;
 }
--- stm32f103/rtconfig.h
+++ stm32f103/rtconfig.h
@@ -145,6 +145,14 @@
 
 /* system packages */
 
+#define PKG_USING_FAL
+#define FAL_DEBUG_CONFIG
+#define FAL_DEBUG 1
+#define FAL_PART_HAS_TABLE_CFG
+#define FAL_USING_SFUD_PORT
+#define FAL_USING_NOR_FLASH_DEV_NAME "norflash0"
+#define PKG_USING_FAL_V00500
+#define PKG_FAL_VER_NUM 0x00500
 
 /* peripheral libraries and drivers */
 
